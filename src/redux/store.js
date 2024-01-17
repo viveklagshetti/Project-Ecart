a7645c18f70ce7ee0cb04a68ef31c56a5a69a282
Vivek Lagshetti <viveklagshetti123@gmail.com>
2024-01-17 15:52:05 +0530

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,13 @@
 // store.js
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import {thunk} from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from './reducer/rootReducers';
-const middleware = [thunk, logger];
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(thunk)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
 );
 
 export default store;
